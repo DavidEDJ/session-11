ca67083859c148756e6b9f50d778aa4f991cef33
DavidEDJ <david.pauget@sciencespo.fr>
2018-11-18 02:48:40 +0100

    Quiz 20NOV
    Quiz 20 nov, 1ere version

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-var vm = new Vue({
-  el: '.app',
-  data: function () {
-    var now = new Date();
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+function check(){
 
-    return  {
-      now: now,
-      day: days[now.getDay()]
-    };
-  }
-})
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+     var correct = 0;
+
+
+	if (question1 == "De Gaulle") {
+		correct++;
+}
+	if (question2 == "Victor Orban") {
+		correct++;
+}	
+	if (question3 == "Francois Mitterrand") {
+		correct++;
+
+}
+	
+
+
+	var pictures = ["obama.gif", "trump.gif", "meluche.gif"];
+	var messages = ["Bravo !", "C'est un peu juste...", "Ce n'est pas glorieux..."];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "Vous avez " + correct + "/3.";
+	document.getElementById("picture").src = pictures[score];
+	}
