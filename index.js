75f86d3ea5e9c7df16c96b4cef582a699a688953
DavidEDJ <david.pauget@sciencespo.fr>
2018-11-18 14:07:06 +0100

    Fin quiz 20NOV  David Pauget
    Fin quiz 20NOV  David Pauget

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
      var correct = 0;
 
 
@@ -17,27 +19,45 @@ function check(){
 
 }
 	
+	if (question4 == "Victor Hugo") {
+		correct++;
+
+}
+	if (question5 == "Francois Asselineau") {
+		correct++;
+
+}
 
 
-	var pictures = ["obama.gif", "trump.gif", "meluche.gif"];
-	var messages = ["Bravo !", "C'est un peu juste...", "Ce n'est pas glorieux..."];
-	var score;
+	var pictures = ["obama.gif", "fillon.gif", "trump.gif", "meluche.gif"];
+	var messages = ["Bravo !",  "Tres bonne maitrise !",  "C'est un peu juste...", "Ce n'est pas glorieux..."];
+	var range;
 
-	if (correct == 0) {
-		score = 2;
+	
+
+	if (correct < 2) {
+		range = 3;
 	}
 
-	if (correct > 0 && correct < 3) {
-		score = 1;
+
+if (correct > 1 && correct < 4) {
+		range = 2;
 	}
 
-	if (correct == 3) {
-		score = 0;
+
+	if (correct == 4) {
+		range = 1;
 	}
+
+
+	if (correct == 5) {
+		range = 0;
+	}
+
 
 	document.getElementById("after_submit").style.visibility = "visible";
 
-	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("number_correct").innerHTML = "Vous avez " + correct + "/3.";
-	document.getElementById("picture").src = pictures[score];
+	document.getElementById("message").innerHTML = messages[range];
+	document.getElementById("number_correct").innerHTML = "Vous avez " + correct + "/5.";
+	document.getElementById("picture").src = pictures[range];
 	}
